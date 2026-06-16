1d99ae94193f65d950b8690c41a9255af481f619
gargreen <a01080723412@gmail.com>
2026-06-17 08:29:17 +0900

    Initial commit

---
+++ src/main/java/com/example/readmesummary/ReadmeSummaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.readmesummary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadmeSummaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadmeSummaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/readmesummary/ReadmeSummaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.readmesummary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadmeSummaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
